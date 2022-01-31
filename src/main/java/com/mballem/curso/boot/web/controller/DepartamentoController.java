410f5ec66a39300e80266ed0c28e000ee1084aa2
Gustavo-F <fgustavo016@gmail.com>
2022-01-31 20:16:32 -0300

    adicionadas mensagens de alerta para departamentos

--- src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
@@ -6,6 +6,7 @@ import com.mballem.curso.boot.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.ui.ModelMap;
 
 @Controller
@@ -27,8 +28,9 @@ public class DepartamentoController {
 	}
 
 	@PostMapping("/salvar")	
-	public String salvar(Departamento departamento) {
+	public String salvar(Departamento departamento, RedirectAttributes attr) {
 		service.salvar(departamento);
+		attr.addFlashAttribute("success", "Departamento inserido com sucesso.");
 
 		return "redirect:/departamentos/cadastrar";
 	}
@@ -40,15 +42,22 @@ public class DepartamentoController {
 	}
 
 	@PostMapping("/editar")
-	public String editar(Departamento departamento) {
+	public String editar(Departamento departamento, RedirectAttributes attr) {
 		service.editar(departamento);
+		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
+		
 		return "redirect:/departamentos/cadastrar";
 	}
 
 	@GetMapping("/excluir/{id}")
 	public String excluir(@PathVariable("id") Long id, ModelMap model) {
-		if (!service.departamentoTemCargos(id))
+		if (service.departamentoTemCargos(id)) {
+			model.addAttribute("fail", "Departamento não removido. Possui cargo(s) vinculado(s)");
+		} else {
 			service.excluir(id);
+			model.addAttribute("success", "Departamento excluído com sucesso.");
+		}
+			
 
 		return listar(model);
 	}
