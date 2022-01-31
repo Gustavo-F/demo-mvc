7a88ae4500db88de9199a4e85c32b5bb6bd3d26e
Gustavo-F <fgustavo016@gmail.com>
2022-01-31 14:43:06 -0300

    init Spring demo-mvc repository

---
+++ src/test/java/com/mballem/curso/boot/DemoMvcApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mballem.curso.boot;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class DemoMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
