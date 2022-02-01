650d8ad9955e2a01a56a7104f1aa21827dbe0530
Gustavo-F <fgustavo016@gmail.com>
2022-02-01 10:13:11 -0300

    adicionada listagem e edição de cargos cadastrados

--- src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
@@ -9,8 +9,10 @@ import com.mballem.curso.boot.service.DepartamentoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -30,7 +32,8 @@ public class CargoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
 		return "/cargo/lista"; 
 	}
 
@@ -39,6 +42,20 @@ public class CargoController {
 		cargoService.salvar(cargo);
 	
 		attr.addFlashAttribute("success", "Cargo cadastrado com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "cargo/cadastro";
+	}
+
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		
+		attr.addFlashAttribute("success", "Cargo atualizado com sucesso.");
 		return "redirect:/cargos/cadastrar";
 	}
 
