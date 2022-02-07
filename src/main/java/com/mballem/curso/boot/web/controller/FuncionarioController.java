6216846d8f6af302d73ad46d1b2786be35162f59
Gustavo-F <fgustavo016@gmail.com>
2022-02-07 11:40:56 -0300

    adicionada validações para funcionário e endereço

--- src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
@@ -3,6 +3,8 @@ package com.mballem.curso.boot.web.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.mballem.curso.boot.domain.Cargo;
 import com.mballem.curso.boot.domain.Funcionario;
 import com.mballem.curso.boot.domain.UF;
@@ -14,6 +16,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,10 +46,13 @@ public class FuncionarioController {
 	}
 
 	@PostMapping("/salvar")
-	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
-		funcionarioService.salvar(funcionario);
+	public String salvar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+		if (result.hasErrors())
+			return "/funcionario/cadastro";
 
+		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário cadastrado com sucesso.");
+
 		return "redirect:/funcionarios/cadastrar";
 	}
 
@@ -57,10 +63,13 @@ public class FuncionarioController {
 	}
 
 	@PostMapping("/editar")
-	public String editar(Funcionario funcionario, RedirectAttributes attr) {
-		funcionarioService.editar(funcionario);
+	public String editar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+		if (result.hasErrors())
+			return "/funcionario/cadastro";
 		
+		funcionarioService.editar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
+		
 		return "redirect:/funcionarios/cadastrar";
 	}
 
