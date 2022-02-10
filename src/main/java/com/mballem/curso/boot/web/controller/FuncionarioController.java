d099a73c618ec94c42ab89c19854009f0387bec8
Gustavo-F <fgustavo016@gmail.com>
2022-02-10 16:32:32 -0300

    correção de URLs de templates e gerado .jar do projeto

--- src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
@@ -44,19 +44,19 @@ public class FuncionarioController {
 
 	@GetMapping("/cadastrar")
 	public String cadastrar(Funcionario funcionario) {
-		return "/funcionario/cadastro";
+		return "funcionario/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar(ModelMap model) {
 		model.addAttribute("funcionarios", funcionarioService.buscarTodos());
-		return "/funcionario/lista"; 
+		return "funcionario/lista"; 
 	}
 
 	@PostMapping("/salvar")
 	public String salvar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
 		if (result.hasErrors())
-			return "/funcionario/cadastro";
+			return "funcionario/cadastro";
 
 		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário cadastrado com sucesso.");
@@ -73,7 +73,7 @@ public class FuncionarioController {
 	@PostMapping("/editar")
 	public String editar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
 		if (result.hasErrors())
-			return "/funcionario/cadastro";
+			return "funcionario/cadastro";
 		
 		funcionarioService.editar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
